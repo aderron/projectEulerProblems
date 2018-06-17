feb1d582cdf68cd351c6e695f6626a452594167f
DerAd <a.derron@wp.pl>
2018-06-17 17:04:13 +0200

    trying to add gitignore

---
+++ problem1/MultiplesOf3And5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using System;
+
+namespace MultiplesOf3And5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+
+            for (int i = 0; i < 1000; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    sum += i;
+                }
+            }
+
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ problem2/evenFibonacciNumbers/evenFibonacciNumbers/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace evenFibonacciNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fibA = 0;
+            int fibB = 1;
+            int result = 0;
+            int sum = 0;
+
+            while (result < 4000000)
+            {
+                result = fibA + fibB;
+                fibA = fibB;
+                fibB = result;
+
+                if (result % 2 == 0)
+                {
+                    sum += result;
+                }
+
+            }
+
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+    }
+}
