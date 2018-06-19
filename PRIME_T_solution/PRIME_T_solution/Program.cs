7d04edb8ce5c39987a47c82d9160b403d2f91559
DerAd <a.derron@wp.pl>
2018-06-19 18:13:08 +0200

    pro

---
+++ PRIME_T_solution/PRIME_T_solution/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace PRIME_T_solution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int a;
+
+            for (int i = 0; i < n; i++)
+            {
+                a = int.Parse(Console.ReadLine());
+
+                if (IsPrime(a) == true)
+                    Console.WriteLine("TAK");
+                else
+                    Console.WriteLine("NIE");
+            }
+
+            Console.ReadLine();
+        }
+
+        static bool IsPrime(int b)
+        {
+            if (b <= 1)
+                return false;
+
+            for (int n = 2; n <= b / 2; n++)
+            {
+                if (b % n == 0)
+                {
+                    return false;
+                }
+
+            }
+
+            return true;
+        }
+    }
+}
